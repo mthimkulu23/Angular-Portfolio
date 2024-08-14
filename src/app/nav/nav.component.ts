9c3ec4c06d9841bcd6b6e52a6c38f0117e53bdcf
mthimkulu23 <146940556+mthimkulu23@users.noreply.github.com>
2024-08-14 12:28:12 +0200

    component array navbar [6]change text insert Resume

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 export class NavComponent {
 home ='Home '
 navbarpro =[
-  'PORTFOLIO','Home', 'About', 'Projects', 'Dropdown' , 'Button'
+  'PORTFOLIO','Home', 'About', 'Projects', 'Dropdown' , 'Resume'
 ]
 
 
